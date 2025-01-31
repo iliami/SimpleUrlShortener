394a2a51d0df0b54707ad9ff50449c8486a5bdbd
iliami <ilia.mi1908@gmail.com>
2025-01-31 18:25:09 +0400

    feature: add simple frontend

--- SimpleUrlShortener.Domain/UrlCodeSettings.cs
+++ SimpleUrlShortener.Domain/UrlCodeSettings.cs
@@ -3,6 +3,6 @@
 public static class UrlCodeSettings
 {
     public const int CodeLength = 7;
-    public const string Alphabet = "qwertyuiopasdfghjklzxcvbnm.,?!0123456789";
+    public const string Alphabet = "qwertyuiopasdfghjklzxcvbnmQWER0123456789";
     public const int MaxAttemptsToResolveCollision = 100;
 }
--- SimpleUrlShortener.Presentation/wwwroot/js/site.js
+++ SimpleUrlShortener.Presentation/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const inputText = document.querySelector(".input-text:disabled");
+const copyButton = document.querySelector("button.button");
+copyButton.addEventListener('click', () => navigator.clipboard.writeText(inputText.value));
